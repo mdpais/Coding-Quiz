29f74412bfd03f2dd890f85a4ac1145bc13c56a8
mdpais <mdpais@gmail.com>
2022-11-17 23:27:35 -0400

    Just the timer remaining

--- quiz.js
+++ quiz.js
@@ -5,15 +5,12 @@ var answerStatus = document.querySelector(".answer-status");
 var cardBody = document.querySelector(".card-body");
 var header = document.querySelector(".timer");
 var footer = document.querySelector(".highscore");
-var score = 0;
-var highScore;
-var form;
-var input = document.createElement("input");
-var submitButton = document.createElement("span");
 
 var storedHighScore = JSON.parse(localStorage.getItem("highScore"));
-
-footer.textContent = "Highest score: " + storedHighScore.scoreStore + " by " + storedHighScore.initialStore + ".";
+if(storedHighScore == null) {
+    storedHighScore = {scoreStore: 0, initialStore: "no one"};
+}
+footer.textContent = "Highest score: " + storedHighScore.scoreStore + " by " + storedHighScore.initialStore;
 
 // Set of questions for the quiz
 var questionsArr = [
@@ -111,6 +108,8 @@ function quiz() {
     }
 }
 
+var score = 0;
+
 // Once an answer option is selected
 options.addEventListener("click", function(event) {
     if(event.target.tagName == "BUTTON") {
@@ -141,19 +140,24 @@ options.addEventListener("click", function(event) {
     }
 });
 
+var highScore;
+var form;
+var input = document.createElement("input");
+var form = document.createElement("form");
+form.setAttribute("class", "mt-2 form");
+var submitButton = document.createElement("span");
+
 function endQuiz() {
+    storedHighScore = JSON.parse(localStorage.getItem("highScore"));
     question.textContent = "Game Over";
     if(score > storedHighScore.scoreStore) {
+        form.textContent = "";
         options.textContent = "You broke the high score record with " + score + " correct answers.";
         // Create form to get initials for score
-        var form = document.createElement("form");
-        form.setAttribute("class", "mt-2");
         var label = document.createElement("label");
         label.textContent = "Enter your initials:";
-        // var input = document.createElement("input");
         input.setAttribute("type", "text");
         input.setAttribute("class", "mx-2");
-        // var submitButton = document.createElement("span");
         submitButton.textContent = "Submit";
         submitButton.setAttribute("class", "btn btn-primary");
         form.appendChild(label);
@@ -163,14 +167,30 @@ function endQuiz() {
     }
     else if(score == storedHighScore.scoreStore) {
         options.textContent = "You tied the high score with " + score + " correct answers. You have to beat it.";
+        okButton();
     }
     else {
         options.textContent = "You only scored " + score + ". Not good enough.";
+        okButton();
     }
 }
 
-submitButton.addEventListener("click", function(event) {
+function okButton() {
+    form.textContent = "";
+    submitButton.textContent = "OK";
+    submitButton.setAttribute("class", "btn btn-primary");
+    form.appendChild(submitButton);
+    cardBody.insertBefore(form, cardBody.children[2]);
+    score = storedHighScore.scoreStore;
+    input.value = storedHighScore.initialStore;
+}
+
+var restartGame = document.createElement("span");
+var resetScore = document.createElement("span");
+
+submitButton.addEventListener("click", function() {
     // create user object from submission
+    form.textContent = "";
     highScore = {
       scoreStore: score,
       initialStore: input.value.trim()
@@ -178,4 +198,32 @@ submitButton.addEventListener("click", function(event) {
     console.log(highScore)
     // set new submission to local storage 
     localStorage.setItem("highScore", JSON.stringify(highScore));
+    question.textContent = "Highest Score achieved in this Game";
+    options.textContent = highScore.scoreStore + " by " + highScore.initialStore;
+    footer.textContent = "Highest score: " + highScore.scoreStore + " by " + highScore.initialStore;
+    restartGame.textContent = "Try Again";
+    restartGame.setAttribute("class", "btn btn-primary mx-2");
+    resetScore.textContent = "Reset High Score";
+    resetScore.setAttribute("class", "btn btn-primary mx-2");
+    form.appendChild(restartGame);
+    form.appendChild(resetScore);
+    cardBody.insertBefore(form, cardBody.children[2]);
+  });
+
+  restartGame.addEventListener("click", function() {
+    options.textContent = "";
+    form.remove();
+    questionIndex = 0;
+    score = 0;
+    quiz();
+  });
+
+  resetScore.addEventListener("click", function() {
+    highScore = {
+        scoreStore: "0",
+        initialStore: "no one"
+      };
+      localStorage.setItem("highScore", JSON.stringify(highScore));
+      options.textContent = highScore.scoreStore + " by " + highScore.initialStore;
+      footer.textContent = "Highest score: " + highScore.scoreStore + " by " + highScore.initialStore;
   });
